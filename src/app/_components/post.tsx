5c95cecfc6a740f78957dcf25159b24cbeb51812
Mohamed Magdy Fayed <mohamed.ahmed.sc2@gmail.com>
2025-12-17 06:25:48 +0200

    megz deployment fix

--- src/app/_components/post.tsx
+++ src/app/_components/post.tsx
@@ -18,11 +18,7 @@ export function LatestPost() {
 
 	return (
 		<div className="w-full max-w-xs">
-			{latestPost ? (
-				<p className="truncate">Your most recent post: {latestPost.name}</p>
-			) : (
-				<p>You have no posts yet.</p>
-			)}
+			<p>You have no posts yet.</p>
 			<form
 				className="flex flex-col gap-2"
 				onSubmit={(e) => {
